f67928f4d7671616995206059152ea14f1649d6b
ankang <ankang@funguide.com.cn>
2017-06-05 09:51:43 +0800

    微信

--- myProject/wenxin/pages/index/index.js
+++ myProject/wenxin/pages/index/index.js
@@ -4,7 +4,9 @@ var app = getApp()
 Page({
   data: {
     motto: '直到世界尽头',
-    userInfo: {}
+    userInfo: {
+
+    }
   },
   //事件处理函数
   bindViewTap: function() {
@@ -14,20 +16,26 @@ Page({
   },
   tapName: function(event) {
     wx.navigateTo({
-     url: '../../swiper/swiper'
+     url: '../swiper/swiper'
+    })
+  },
+  primary: function(event) {
+    wx.navigateTo({
+     url: '../secondpages/index1'
     })
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
+    console.log('onLoad');
+    var that = this;
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
       })
-    })
+    });
+    app.setNavigationBarTitle({
+      title: '当前页面'
+    });
   }
 })
-
-
---
+++ myProject/wenxin/pages/secondpages/index1.js
@@ -0,0 +1,55 @@
+// pages/secondpages/index1.js
+Page({
+  data:{
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-09-15'
+    },
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+    showNavigationBarLoading();
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  },
+})
